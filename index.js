2ea12ea0d1f3c2fafaa862cd48db3ed2e0d93f90
Clinton <92377226+mywebreview@users.noreply.github.com>
2022-12-13 07:56:08 +1300

    Newly Written Codes

---
+++ index.js
@@ -0,0 +1,10 @@
+var mainListDiv = document.getElementById("navBar"),
+    mediaButton = document.getElementById("icon");
+
+mediaButton.onclick = function () {
+    
+    "use strict";
+    
+    navBar.classList.toggle("show_list");
+    icon.classList.toggle("active");
+};
